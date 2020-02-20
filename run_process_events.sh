3a2065f84c46ef9a61b57e4ca5bbf120583e7e97
Katie <kmurley@gmail.com>
2020-02-20 17:07:30 +0100

    .

--- run_process_events.sh
+++ run_process_events.sh
@@ -5,18 +5,15 @@
 use root
 
 python process_V2.py -f 20200109_2217
-
-
-python process_V2.py 20200109_2217
-python process_V2.py 20200110_0017
-python process_V2.py 20200111_0017
-python process_V2.py 20200112_0017
-python process_V2.py 20200113_0017
-python process_V2.py 20200113_2228
-python process_V2.py 20200114_0028
-python process_V2.py 20200115_0028
-python process_V2.py 20200115_0922
-python process_V2.py 20200115_1436
-python process_V2.py 20200116_0036
-python process_V2.py 20200119_2211
-python process_V2.py 20200120_0011
+python process_V2.py -f 20200110_0017
+python process_V2.py -f 20200111_0017
+python process_V2.py -f 20200112_0017
+python process_V2.py -f 20200113_0017
+python process_V2.py -f 20200113_2228
+python process_V2.py -f 20200114_0028
+python process_V2.py -f 20200115_0028
+python process_V2.py -f 20200115_0922
+python process_V2.py -f 20200115_1436
+python process_V2.py -f 20200116_0036
+python process_V2.py -f 20200119_2211
+python process_V2.py -f 20200120_0011

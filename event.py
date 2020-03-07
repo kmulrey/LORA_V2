2059518abbb233ea12723d850a503fba9c34459a
Katie <kmurley@gmail.com>
2020-03-07 23:36:30 +0100

    .

--- event.py
+++ event.py
@@ -61,7 +61,7 @@ def read_attenuation():
         return data
     except:
         print('problem reading atm file')
-        print('test')
+        print('test23')
         return 0
 
 def find_counts(detector):

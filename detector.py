a31a31cf9c525be08a8b06bc85550dc899d09c0e
Katie <kmurley@gmail.com>
2020-03-07 23:51:19 +0100

    .

--- detector.py
+++ detector.py
@@ -190,15 +190,13 @@ def load_sec_information(info0,info1,info2,lasas,V):
 
 def load_positions(detectors):
     file=open(LORA.det_cord_file,'r')
-    print(LORA.det_cord_file)
-    '''
     cordinates=np.genfromtxt(file,skip_header=2,usecols=(1,2,3))
     file.close()
     for i in np.arange(LORA.nDetB):
         detectors[i].x_cord=cordinates[i][0]
         detectors[i].y_cord=cordinates[i][1]
         detectors[i].z_cord=cordinates[i][2]
-    '''
+    
 
 
 def load_signal(detectors):

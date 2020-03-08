92398fbf2fba4f5d62c735c6a05ac9e5f268851f
Katie <kmurley@gmail.com>
2020-03-08 11:32:41 +0100

    .

--- event.py
+++ event.py
@@ -242,10 +242,9 @@ def get_event_timestamp_V2(detector,lasa):
                 #print detector.event_time_stamp
                 
     else:
-        print('lasa flag: {0}'.format(lasa.sec_flag))
         if lasa.sec_flag!=1:
             detector.event_time_stamp=10*(lasa.sync[0]+lasa.quant[1]+(1.0*detector.ctd/lasa.CTP[1])*(1000000000.0-lasa.quant[1]+lasa.quant[2]))
-            #print detector.event_time_stamp
+            print(lasa.sync[0],lasa.quant[1],detector.ctd,detector.event_time_stamp,lasa.CTP[1],lasa.quant[2])
         else:
             #print 'flagged event'
             detector.event_time_stamp=10*detector.nsec

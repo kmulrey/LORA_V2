30c053fa861bbf76e55d93abda3e96315e4c388f
Katie <kmurley@gmail.com>
2021-11-08 13:11:06 +0100

    .

--- event.py
+++ event.py
@@ -211,10 +211,10 @@ def get_arrival_time(detector):
         
         
         flag=0
-        #print('number {0},counts {1}'.format(detector.number,detector.trace_int_counts))
+        print('number {0},counts {1}'.format(detector.number,detector.trace_int_counts))
 
         for i in np.arange(LORA.nTrace):
-            print(detector.threshold_time_no_trig)
+            #print(detector.threshold_time_no_trig)
             if -1*detector.counts[i]>(45) and flag==0:
                 if i>50:
                     detector.threshold_time_no_trig=i*5.0*10
